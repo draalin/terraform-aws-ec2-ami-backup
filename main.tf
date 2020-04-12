bfa310821f7b1acd391ea252a2a5465134c3f657
Tyler Bailey <tbailey0316@gmail.com>
2020-04-12 09:33:57 -0400

    Remove local module from archive_file resource

--- main.tf
+++ main.tf
@@ -50,14 +50,14 @@ data "aws_iam_policy_document" "ami_backup" {
 
 data "archive_file" "ami_backup" {
   type        = "zip"
-  source_file = "${local.module_relpath}/ami_backup.py"
-  output_path = "${local.module_relpath}/ami_backup.zip"
+  source_file = "ami_backup.py"
+  output_path = "ami_backup.zip"
 }
 
 data "archive_file" "ami_cleanup" {
   type        = "zip"
-  source_file = "${local.module_relpath}/ami_cleanup.py"
-  output_path = "${local.module_relpath}/ami_cleanup.zip"
+  source_file = "ami_cleanup.py"
+  output_path = "ami_cleanup.zip"
 }
 
 module "label" {
